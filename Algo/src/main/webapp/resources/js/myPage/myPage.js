7653c3a110651156f2d059e92890f33e56967bf9
DESKTOP-835L1KV\user1 <eunmi080163@gmail.com>
2024-01-26 17:13:50 +0900

    1/26 마이페이지js 추가

--- Algo/src/main/webapp/resources/js/myPage/myPage.js
+++ Algo/src/main/webapp/resources/js/myPage/myPage.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 window.onload = () => {
 	let lm_btn1 = document.getElementById('lm_btn1');
 	
@@ -7,11 +6,4 @@ window.onload = () => {
 	window.open('location/window', '_blank', 'width=900px,height=1000px');
 	});
 }
-=======
-var lm_btn1 = document.getElementById('lm_btn1');
-
-lm_btn1.addEventListener('click', function() {
-window.open('location/window', '_blank', 'width=860,height=600');
-});
->>>>>>> junseok
 
